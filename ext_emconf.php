1a7e25c9cac137dff1ce6892d38ae5378a132efd
Christian Kuhn <lolli@schwarzbu.ch>
2019-01-20 14:22:24 +0100

    [TASK] Update composer.json and ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,18 +1,18 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => 'User>Task Center',
-    'description' => 'The Task Center is the framework for a host of other extensions, see below.',
+    'description' => 'The Task Center is the framework for a host of other extensions.',
     'category' => 'module',
     'state' => 'stable',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'author' => 'TYPO3 Core Team',
-    'author_email' => 'typo3cms@typo3.org',
+    'author' => 'Friends of TYPO3',
+    'author_email' => 'friendsof@typo3.org',
     'author_company' => '',
     'version' => '10.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.0.0',
+            'typo3' => '10.0.0-10.9.99',
         ],
         'conflicts' => [],
         'suggests' => [
